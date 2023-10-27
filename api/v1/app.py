22e95b04f5dd9744c7e1e5c25f002dfb21058e7a
alidrisy <a.alidrisy6@egamil.com>
2023-10-27 14:31:38 +0300

    fix error

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
 
 if getenv('HBNB_API_HOST'):
     host = getenv('HBNB_API_HOST')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from flask import jsonify, make_response
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def states():
     """ Returns a JSON status """
     return make_response(jsonify({"status": "OK"}))
