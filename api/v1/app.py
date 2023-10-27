b7198b000993f1c8f2776a0a0180c453491f5c6d
alidrisy <a.alidrisy6@egamil.com>
2023-10-27 11:51:46 +0300

    4. Some stats?

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Flask web app """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -17,6 +17,11 @@ if getenv('HBNB_API_PORT'):
 else:
     port = 5000
 
+
+@app.errorhandler(404)
+def not_found_error(exception):
+    """ “404 page”, a “Not found” """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 @app.teardown_appcontext
 def teardown(exception):
