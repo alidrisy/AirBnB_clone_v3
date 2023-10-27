58beb519043d774ad5d6aa0e05cec7a1b9bf0e6d
alidrisy <a.alidrisy6@egamil.com>
2023-10-27 23:06:01 +0300

    make it executable

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -16,22 +16,20 @@ def get_users(user_id=None):
         return jsonify(users_list)
     else:
         user = storage.get(User, user_id)
-        if user:
-            return jsonify(user.to_dict())
-        else:
+        if not user:
             abort(404)
+        return jsonify(user.to_dict())
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
     """Deletes a user based on its ID"""
     user = storage.get(User, user_id)
-    if user:
-        storage.delete(user)
-        storage.save()
-        return make_response(jsonify({}), 200)
-    else:
+    if not user:
         abort(404)
+    storage.delete(user)
+    storage.save()
+    return make_response(jsonify({}), 200)
 
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
@@ -53,15 +51,14 @@ def create_user():
 def update_user(user_id):
     """Updates a user using a PUT request"""
     user = storage.get(User, user_id)
-    if user:
-        data = request.get_json()
-        if not data:
-            abort(400, "Not a JSON")
-
-        for key, value in data.items():
-            if key not in ["email", "id", "created_at", "updated_at"]:
-                setattr(user, key, value)
-        user.save()
-        return make_response(jsonify(user.to_dict()), 200)
-    else:
+    if not user:
         abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, "Not a JSON")
+
+    for key, value in data.items():
+        if key not in ["email", "id", "created_at", "updated_at"]:
+            setattr(user, key, value)
+    user.save()
+    return make_response(jsonify(user.to_dict()), 200)
