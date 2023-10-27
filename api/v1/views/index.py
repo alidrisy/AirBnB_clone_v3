36651213d37616139abdc7751e2aecc2d8decd5a
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-10-27 05:06:06 -0700

    adding the stats endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,30 @@
 """ Flask web templet """
 from api.v1.views import app_views
 from flask import jsonify, make_response
+from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/status', strict_slashes=False)
 def states():
     """ Returns a JSON status """
     return make_response(jsonify({"status": "OK"}))
+
+@app_views.route('/stats', strict_slashes=False)
+def print_count():
+    """Return the count of all classes in json format"""
+    stats = {}
+
+    class_names = {
+            "states": "State",
+            "cities": "City",
+            "amenities": "Amenity",
+            "places": "Place",
+            "reviews": "Review",
+            "users": "User"
+            }
+
+    for class_name in class_names:
+        stats[class_name] = storage.count(class_names[class_name])
+
+    return make_response(jsonify(stats))
