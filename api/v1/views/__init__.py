557bcf13247dc60e09a7a54f92c2bf436a8598ae
alidrisy <a.alidrisy6@egamil.com>
2023-10-27 21:52:59 +0300

    10. Place

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Views model """
 from flask import Blueprint
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
@@ -9,3 +8,4 @@ import api.v1.views.states
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
+from api.v1.views.places import *
