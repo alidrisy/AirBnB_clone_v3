c2ec30579bcd95eec63d4f1dad67123b268e0c33
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-10-28 04:55:37 -0700

    changing the way of import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,9 +4,9 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
-import api.v1.views.states
+from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
-import api.v1.views.users
+from api.v1.views.users import *
 from api.v1.views.places import *
-import api.v1.views.places_reviews
+from api.v1.views.places_reviews import *
